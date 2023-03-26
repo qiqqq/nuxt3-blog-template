e2c14b561762b5b35e156c82923750c96d9d2e74
Tomasz Kikowski <tomaszkikowski@gmail.com>
2023-03-26 17:33:22 +0200

    Creating single blog page

--- pages/[[slug]].vue
+++ pages/[[slug]].vue
@@ -1,3 +1,46 @@
 <template>
-  <div>Single post page</div>
+  <article>
+    <h1 class="my-6 text-center font-serif">
+      {{ post.title }}
+    </h1>
+    <div class="my-8 text-center font-serif text-base text-gray">
+      <time>{{ post.date }}</time> &mdash; {{ post.author }}
+    </div>
+    <img
+      :src="post.image"
+      :alt="post.title"
+      class="rounded block my-10"
+    />
+    <div class="max-w-3xl mx-auto my-8">
+      <ContentRenderer 
+        class="content font-serif text-lg"
+        :value="post" 
+      />
+      <div class="sm:flex justify-between items-center mt-16">
+        <div class="flex items-center">
+          <img :src="post.avatar" :alt="post.author" class="w-12 h-12 rounded-full">
+          <div class="ml-4">
+            <strong class="block">{{ post.author }}</strong>
+            <span>{{ post.date }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </article>
 </template>
+
+<script>
+import { defineComponent } from '@vue/composition-api';
+import { useRoute } from "vue-router";
+
+export default defineComponent({
+  async setup() {
+    const route = useRoute()
+    const { data:post } = await useAsyncData('posts', () => queryContent(route.path).findOne())
+
+    return {
+      post
+    }
+  },
+})
+</script>
